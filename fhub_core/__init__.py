a6c60a1d0f90cfecbf2c19e369a28200d8aa124d
Micah Smith <micahjsmith@gmail.com>
2018-04-11 16:24:09 -0400

    Update exports

--- fhub_core/__init__.py
+++ fhub_core/__init__.py
@@ -6,4 +6,5 @@ __author__ = """Micah Smith"""
 __email__ = 'micahs@mit.edu'
 __version__ = '0.1.0'
 
-from .feature import *  # noqa
+from fhub_core.feature import *  # noqa
+from fhub_core.contrib import *  # noqa
