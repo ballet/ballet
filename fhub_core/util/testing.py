562f99baf1797a6408f3f7cd4db8e1291924a68a
Micah Smith <micahjsmith@gmail.com>
2018-04-10 21:32:16 -0400

    Add not equal assertions

--- fhub_core/util/testing.py
+++ fhub_core/util/testing.py
@@ -18,6 +18,17 @@ class ArrayLikeEqualityTestingMixin:
             msg = self._formatMessage(msg, standardMsg)
             raise self.failureException(msg)
 
+    def assertArrayNotEqual(self, first, second, msg=None):
+        try:
+            npt.assert_array_equal(first, second, verbose=False)
+        except AssertionError:
+            pass
+        else:
+            standardMsg = '{} == {}'.format(
+                *_common_shorten_repr(first, second))
+            msg = self._formatMessage(msg, standardMsg)
+            raise self.failureException(msg)
+
     def assertArrayAlmostEqual(
             self, first, second, places=6, msg=None, delta=None):
 
@@ -39,6 +50,22 @@ class ArrayLikeEqualityTestingMixin:
             msg = self._formatMessage(msg, standardMsg)
             raise self.failureException(msg)
 
+    def assertArrayNotAlmostEqual(
+            self, first, second, places=6, msg=None, delta=None):
+        if delta is not None:
+            places = int(-np.log10(delta) + np.log10(1.5))
+
+        try:
+            npt.assert_array_almost_equal(
+                first, second, decimal=places, verbose=False)
+        except AssertionError:
+            pass
+        else:
+            standardMsg = '{} == {}'.format(
+                *_common_shorten_repr(first, second))
+            msg = self._formatMessage(msg, standardMsg)
+            raise self.failureException(msg)
+
     def _assertPandasEqual(self, func, first, second, msg=None, **kwargs):
         try:
             func(first, second, **kwargs)
@@ -48,16 +75,39 @@ class ArrayLikeEqualityTestingMixin:
             msg = self._formatMessage(msg, standardMsg)
             raise self.failureException(msg)
 
+    def _assertPandasNotEqual(self, func, first, second, msg=None, **kwargs):
+        try:
+            func(first, second, **kwargs)
+        except AssertionError:
+            pass
+        else:
+            standardMsg = '{} == {}'.format(
+                *_common_shorten_repr(first, second))
+            msg = self._formatMessage(msg, standardMsg)
+            raise self.failureException(msg)
+
     def assertFrameEqual(self, first, second, msg=None, **kwargs):
         self._assertPandasEqual(
+            pdt.assert_frame_equal, first, second, msg=None, **kwargs)
+
+    def assertFrameNotEqual(self, first, second, msg=None, **kwargs):
+        self._assertPandasNotEqual(
             pdt.assert_frame_equal, first, second, msg=None, **kwargs)
 
     def assertSeriesEqual(self, first, second, msg=None, **kwargs):
         self._assertPandasEqual(
             pdt.assert_series_equal, first, second, msg=None, **kwargs)
 
+    def assertSeriesNotEqual(self, first, second, msg=None, **kwargs):
+        self._assertPandasNotEqual(
+            pdt.assert_series_equal, first, second, msg=None, **kwargs)
+
     def assertIndexEqual(self, first, second, msg=None, **kwargs):
         self._assertPandasEqual(
+            pdt.assert_index_equal, first, second, msg=None, **kwargs)
+
+    def assertIndexNotEqual(self, first, second, msg=None, **kwargs):
+        self._assertPandasNotEqual(
             pdt.assert_index_equal, first, second, msg=None, **kwargs)
 
     def assertPandasObjectEqual(self, first, second, msg=None, **kwargs):
@@ -72,9 +122,26 @@ class ArrayLikeEqualityTestingMixin:
                     self.assertIndexEqual(first, second, msg=msg, **kwargs)
                 else:
                     # unreachable?
-                    pass
+                    raise AssertionError('you found a bug: unreachable code')
         else:
             standardMsg = '{} and {} are uncomparable types'.format(
                 *_common_shorten_repr(first, second))
             msg = self._formatMessage(msg, standardMsg)
             raise self.failureException(msg)
+
+    def assertPandasObjectNotEqual(self, first, second, msg=None, **kwargs):
+        is_pdobj = funcy.isa(pd.core.base.PandasObject)
+        if is_pdobj(first) and is_pdobj(second):
+            if isinstance(first, type(second)):
+                if isinstance(first, pd.DataFrame):
+                    self.assertFrameNotEqual(first, second, msg=msg, **kwargs)
+                elif isinstance(first, pd.Series):
+                    self.assertSeriesNotEqual(first, second, msg=msg, **kwargs)
+                elif isinstance(first, pd.Index):
+                    self.assertIndexNotEqual(first, second, msg=msg, **kwargs)
+                else:
+                    # unreachable?
+                    raise AssertionError('you found a bug: unreachable code')
+        else:
+            # it's great that they are uncomparable types :)
+            pass
