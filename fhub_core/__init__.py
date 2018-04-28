68274ce8e12cafb22c6a19528410d42ac0082e50
Micah Smith <micahjsmith@gmail.com>
2018-04-28 11:20:46 -0400

    Bump version: 0.2.3 → 0.2.4

--- fhub_core/__init__.py
+++ fhub_core/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Micah Smith"""
 __email__ = 'micahs@mit.edu'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from fhub_core.feature import *  # noqa
 from fhub_core.contrib import *  # noqa
