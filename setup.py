5c594dd45d455d2dec602e03275e91144a74ab98
Micah Smith <micahjsmith@gmail.com>
2018-04-30 07:46:50 -0400

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/HDI-Project/fhub_core',
-    version='0.3.3',
+    version='0.3.4',
     zip_safe=False,
 )
