d07f462220d18d3396f7550afc5f9013656b3cf2
Micah Smith <micahjsmith@gmail.com>
2018-09-27 14:00:09 -0400

    Guard convenience log handler against re-initialization

--- ballet/util/log.py
+++ ballet/util/log.py
@@ -2,18 +2,27 @@ import logging
 
 import ballet
 
-logger = logging.getLogger(ballet.__name__)
 
 LOG_FORMAT = r'[%(asctime)s] {%(name)s: %(filename)s:%(lineno)d} %(levelname)s - %(message)s'  # noqa E501
 SIMPLE_LOG_FORMAT = r'%(asctime)s %(levelname)s - %(message)s'
 
+logger = logging.getLogger(ballet.__name__)
+_handler = None
+
 
 def enable(level=logging.INFO):
     """Enable simple console logging for this module"""
-    logger.setLevel(logging.DEBUG)
-    stderr_handler = logging.StreamHandler()
-    stderr_handler.setLevel(level)
-    formatter = logging.Formatter(LOG_FORMAT)
-    stderr_handler.setFormatter(formatter)
-    logger.addHandler(stderr_handler)
-    logger.info('Logging enabled.')
+    name=logging._levelToName[level]
+    global _handler
+    if _handler is None:
+        logger.setLevel(level)
+        _handler = logging.StreamHandler()
+        _handler.setLevel(level)
+        formatter = logging.Formatter(LOG_FORMAT)
+        _handler.setFormatter(formatter)
+        logger.addHandler(_handler)
+        logger.log(level, 'Logging enabled at level {name}.'.format(name=name))
+    else:
+        logger.setLevel(level)
+        _handler.setLevel(level)
+        logger.log(level, 'Logging changed to level {name}.'.format(name=name))
