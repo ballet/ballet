131358eac2021268ed5dbb8360ad131d675d21b0
Micah Smith <micahjsmith@gmail.com>
2018-09-27 14:14:10 -0400

    Improve robust pipeline and update logging

--- ballet/feature.py
+++ ballet/feature.py
@@ -1,11 +1,12 @@
+import sys
 import traceback
 
-import funcy
 import numpy as np
 import pandas as pd
+from funcy import identity, is_seqcont, wraps
 from sklearn.pipeline import _name_estimators
 from sklearn_pandas import DataFrameMapper
-from sklearn_pandas.pipeline import TransformerPipeline
+from sklearn_pandas.pipeline import TransformerPipeline, make_transformer_pipeline
 
 from ballet.util import asarray2d, indent
 from ballet.util.log import logger
@@ -27,42 +28,67 @@ def make_mapper(features):
         input_df=True)
 
 
-class RobustTransformerPipeline(TransformerPipeline):
-
-    def transform(self, X, **transform_kwargs):
-        _transform = make_robust_to_tabular_types(super().transform)
-        return _transform(X, **transform_kwargs)
-
-
-def make_robust_transformer_pipeline(*steps):
-    '''Construct a RobustTransformerPipeline from the given estimators.'''
-    return RobustTransformerPipeline(_name_estimators(steps))
+default_caught = (ValueError, TypeError)
+conversion_approaches = [
+    ('identity', identity, default_caught),
+    ('series', pd.Series, default_caught),
+    ('dataframe', pd.DataFrame, default_caught),
+    ('array', np.asarray, default_caught),
+    ('asarray2d', asarray2d, ()),
+]
 
 
-def make_conversion_approaches():
-    funcs = (funcy.identity, pd.Series, pd.DataFrame, np.asarray, asarray2d)
-    catch = (ValueError, TypeError)
-    for func in funcs[:-1]:
-        yield func, catch
-    yield funcs[-1], ()
+def log_failure_detail(failure_detail):
+    print('*** logging is happening', flush=True)
+    logger.info("Conversion failed, and we're not sure why...")
+    logger.info('Here are the approaches we tried, and how they failed.')
+    for i, item in enumerate(failure_detail):
+        msg = ('{i}. Conversion using approach {name!r} resulted in '
+               '{exc_name} ({exc}), ')
+        if item['outcome'] == 'caught':
+            msg += 'which was caught, and the next approach was tried.'
+        elif item['outcome'] == 'failure':
+            msg += 'which was an unexpected error resulting in failure.'
+        else:
+            raise RuntimeError
+        exc_name = type(item['exc']).__name__
+        logger.info(msg.format(i=i+1, exc_name=exc_name, **item))
 
 
 def make_robust_to_tabular_types(func):
-    @funcy.wraps(func)
+
+    @wraps(func)
     def wrapped(X, y=None, **kwargs):
-        for convert, catch in make_conversion_approaches():
+        failure_detail = []
+        for name, convert, caught in conversion_approaches:
             try:
                 logger.debug(
-                    "Converting using approach '{}'".format(convert.__name__))
+                    "Converting using approach '{name}'"
+                    .format(name=name))
                 if y is not None:
                     return func(convert(X), y=convert(y), **kwargs)
                 else:
                     return func(convert(X), **kwargs)
-            except catch as e:
-                formatted_exc = indent(traceback.format_exc(), n=8)
+            except caught as e:
+                tb = traceback.format_exc()
+                failure_detail.append(
+                    {'name': name, 'exc': e, 'tb': tb, 'outcome': 'caught'})
+                pretty_tb = indent(tb, n=8)
                 logger.debug(
-                    "Application subsequently failed with exception '{}'\n\n{}"
-                    .format(e.__class__.__name__, formatted_exc))
+                    "Conversion approach didn't work, "
+                    "caught exception '{name}'\n\n{tb}"
+                    .format(name=type(e).__name__, tb=pretty_tb))
+                continue
+            except Exception as e:
+                tb = traceback.format_exc()
+                failure_detail.append(
+                    {'name': name, 'exc': e, 'tb': tb, 'outcome': 'failure'})
+                log_failure_detail(failure_detail)
+                raise
+
+        # none of the conversion attempts succeeded
+        log_failure_detail(failure_detail)
+
     return wrapped
 
 
@@ -72,11 +98,19 @@ def make_robust_transformer(transformer):
     return transformer
 
 
+def make_robust_transformer_pipeline(*steps):
+    '''Construct a RobustTransformerPipeline from the given estimators.'''
+    transformer_pipeline = make_transformer_pipeline(*steps)
+    for i in range(transformer_pipeline.steps):
+        transformer_pipeline.steps[i][1] = make_robust_transformer(transformer_pipeline.steps[i][1])
+    return transformer_pipeline
+
+
 class Feature:
     def __init__(self, input, transformer, name=None, description=None,
                  output=None, source=None, options=None):
         self.input = input
-        if funcy.is_seqcont(transformer):
+        if is_seqcont(transformer):
             transformer = make_robust_transformer_pipeline(*transformer)
         self.transformer = make_robust_transformer(transformer)
         self.name = name
