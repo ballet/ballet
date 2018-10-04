b06753a310091b22e32be9d2f27d4ed2682dbd58
Micah Smith <micahjsmith@gmail.com>
2018-10-04 14:53:37 -0400

    Merge multiple config files together

--- ballet/project.py
+++ ballet/project.py
@@ -1,23 +1,33 @@
 import yaml
 from ballet.compat import pathlib
 from ballet.exc import ConfigurationError
-from funcy import get_in, partial
+from funcy import get_in, merge, partial
 
 
 def find_ballet_yml(package_root):
+    """Find ballet.yml config file
+
+    Recursively search up the directory tree from the package root, and then also search in
+    """
     package_root = pathlib.Path(package_root)
     dirs = list(package_root.parents) + [pathlib.Path.home().joinpath('.ballet')]
+    configs = []
     for d in dirs:
         candidate = d.joinpath('ballet.yml')
         if candidate.exists() and candidate.is_file():
             with candidate.open('r') as f:
-                return yaml.load(f)
+                config = yaml.load(f)
+                configs.append(config)
 
-    raise ConfigurationError
+    if configs:
+        return configs
+    else:
+        raise ConfigurationError("Couldn't find any ballet.yml config files.")
 
 
 def config_get(package_root, *path, default=None):
-    config = find_ballet_yml(package_root)
+    configs = find_ballet_yml(package_root)
+    config = merge(*configs)
     return get_in(config, path, default=default)
 
 
