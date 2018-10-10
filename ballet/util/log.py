f9ade8542adb4d3d85d70ae97afcc6b83a610018
Micah Smith <micahjsmith@gmail.com>
2018-10-10 10:56:38 -0400

    Add stacklog

--- ballet/util/log.py
+++ ballet/util/log.py
@@ -1,5 +1,7 @@
 import logging
 
+from funcy import wraps
+
 import ballet
 
 LOG_FORMAT = r'[%(asctime)s] {%(name)s: %(filename)s:%(lineno)d} %(levelname)s - %(message)s'  # noqa E501
@@ -55,4 +57,67 @@ class LoggingContext(object):
             self.logger.removeHandler(self.handler)
         if self.handler and self.close:
             self.handler.close()
-        # implicit return of None => don't swallow exceptions
+        return None
+
+
+class stacklog:
+    """Stack log messages
+
+    Args:
+        method: log callable
+        message: log message
+        *args: args to log method
+        **kwargs: kwargs to log method
+
+    Example usage:
+
+       with stacklog(logging.info, 'Running long function'):
+           run_long_function()
+
+       with stacklog(logging.info, 'Running error-prone function'):
+           raise Exception
+
+    This produces logging output:
+
+        INFO:root:Running long function...
+        INFO:root:Running long function...DONE
+        INFO:root:Running error-prone function...
+        INFO:root:Running error-prone function...FAILURE
+    """
+
+    def __init__(self, method, message, *args, **kwargs):
+        self.method = method
+        self.message = str(message)
+        self.args = args
+        self.kwargs = kwargs
+
+    def _log(self, suffix=''):
+        self.method(self.message + '...' + suffix, *self.args, **self.kwargs)
+
+    _begin = _log
+
+    def _succeed(self):
+        self._log(suffix='DONE')
+
+    def _fail(self):
+        self._log(suffix='FAILURE')
+
+    def __call__(self, func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    def __enter__(self):
+        self._begin()
+        return
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self._succeed()
+        else:
+            self._fail()
+        return False
