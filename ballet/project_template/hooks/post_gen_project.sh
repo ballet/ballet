efe219c7349bfe5c42127320f02af43c071916a1
Micah Smith <micahjsmith@gmail.com>
2018-10-12 10:41:07 -0400

    Do git local config in post_gen_project

--- ballet/project_template/hooks/post_gen_project.sh
+++ ballet/project_template/hooks/post_gen_project.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 git init
+git config --local --add user.name "{{ cookiecutter.user_name }}"
+git config --local --add user.email "{{ cookiecutter.email }}"
+git config --local --add github.user "{{ cookiecutter.github_user }}"
 git add .
 git commit -m "Automatically generated files from ballet-quickstart"
+git remote add origin "https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}"
+git branch --set-upstream-to origin/master
