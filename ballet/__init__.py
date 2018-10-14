c1867a0cad69c2e2157031c322264826ff310696
Micah Smith <micahjsmith@gmail.com>
2018-10-14 19:57:15 -0400

    Bump version: 0.4.1 → 0.5.0

--- ballet/__init__.py
+++ ballet/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Micah Smith"""
 __email__ = 'micahs@mit.edu'
-__version__ = '0.4.1'
+__version__ = '0.5.0'
 
 
 # re-export some names
--- setup.py
+++ setup.py
@@ -95,6 +95,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/HDI-Project/ballet',
-    version='0.4.1',
+    version='0.5.0',
     zip_safe=False,
 )
