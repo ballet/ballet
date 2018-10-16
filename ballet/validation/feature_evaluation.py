686823bc79b8f2924937540fb857ab80fc5a3a24
Micah Smith <micahjsmith@gmail.com>
2018-10-16 17:11:38 -0400

    Add no-op feature evaluation classes

--- ballet/validation/feature_evaluation.py
+++ ballet/validation/feature_evaluation.py
@@ -2,21 +2,13 @@ from ballet.validation.base import (
     FeatureAcceptanceEvaluator, FeaturePruningEvaluator)
 
 
-class FeatureRelevanceEvaluator(FeatureAcceptanceEvaluator):
-    """Accept a feature if it is correlated to the target"""
+class NoOpAcceptanceEvaluator(FeatureAcceptanceEvaluator):
 
     def judge(self, feature):
         return True
 
 
-class FeatureRedundancyEvaluator(FeaturePruningEvaluator):
-    """Remove a feature if it is conditionally independent of the target
-
-    Let Sk be the set of subsets of features of size less than or equal to k.
-    A feature Xi is redundant if it is independent of the target, conditional
-    on some S in Sk. If a feature is redundant, it is removed from the feature
-    matrix.
-    """
+class NoOpPruningEvaluator(FeaturePruningEvaluator):
 
     def prune(self, k=4):
         return []
