7f4bd5b0da05e4afc843ac4db7c163d60bfb2802
Micah Smith <micahjsmith@gmail.com>
2018-10-16 09:10:02 -0400

    Update project template

--- ballet/project_template/hooks/post_gen_project.sh
+++ ballet/project_template/hooks/post_gen_project.sh
@@ -3,7 +3,7 @@
 git init
 git config --local --add user.name "{{ cookiecutter.full_name }}"
 git config --local --add user.email "{{ cookiecutter.email }}"
-git config --local --add github.user "{{ cookiecutter.github_username }}"
+git config --local --add github.user "{{ cookiecutter.github_owner }}"
 git add .
 git commit -m "Automatically generated files from ballet-quickstart"
-git remote add origin "https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}"
+git remote add origin "https://github.com/{{ cookiecutter.github_owner }}/{{ cookiecutter.project_slug }}"
