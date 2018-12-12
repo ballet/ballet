f63757d0daf79f917981364d45f986bfe001a2c3
Kelvin Lu <kelvinlu@mit.edu>
2018-12-12 14:24:03 -0500

    fix type error

--- ballet/compat.py
+++ ballet/compat.py
@@ -13,8 +13,7 @@ import contextlib
 redirect_stdout = contextlib.redirect_stdout
 if sys.version_info < (3, 5, 0):
     import pathlib2 as pathlib
-    def redirect_stderr(path):
-        f = open(path, 'w')
+    def redirect_stderr(f):
         sys.stderr = f
 else:
     import pathlib  # noqa F401
