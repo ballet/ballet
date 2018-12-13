464ce6c6005fcd8555d76d9d20d1265f64da6ff2
Micah Smith <micahjsmith@gmail.com>
2018-12-12 21:59:13 -0500

    Restore old stream in compat redirect_stderr

--- ballet/compat.py
+++ ballet/compat.py
@@ -8,19 +8,29 @@ except ImportError:
 
 # pathlib compatibility
 import sys
-import contextlib
 
-redirect_stdout = contextlib.redirect_stdout
 if sys.version_info < (3, 5, 0):
     import pathlib2 as pathlib
-    def redirect_stderr(f):
-        sys.stderr = f
-        return f
 else:
     import pathlib  # noqa F401
-    redirect_stderr = contextlib.redirect_stderr
+
 if sys.version_info < (3, 6, 0):
     safepath = str
 else:
     from funcy import identity
     safepath = identity
+
+from contextlib import redirect_stdout  # noqa F401
+try:
+    from contextlib import redirect_stderr
+except ImportError:
+    from contextlib import contextmanager
+    @contextmanager
+    def redirect_stderr(f):
+        oldstream = sys.stderr
+        try:
+            sys.stderr = f
+            yield f
+        finally:
+            sys.stderr = oldstream
+
