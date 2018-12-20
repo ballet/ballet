8a393b79bfa3464c7e54d51ca4bf01307e61b89f
Kelvin Lu <kelvinlu@mit.edu>
2018-12-20 16:29:11 -0500

    Create box cox transformer (#15)
    * Create box cox transformer
    
    * lambda -> lmbda
    
    * fix code, as according to comments - thanks micah!
    
    * isort linting
    
    * return self
    
    * create test misc, fix error
    
    * don't include vscode'
    
    * partial: fix for different types
    
    * hack to fix columns for pd.Dataframe
    
    * test cases, absolute skew
    
    * fix failing test
    
    * round on np array
    
    * use around instead of round
    
    * fix wrong test
    
    * fix unskewed/skewed series test, correct rounding
    
    * working tests for box cox transformer
    
    * address comments, better checks and return values
    
    * keep unskewed columns
    
    * fix broken test
    
    * fix numpy case
    
    * Clean up boxcox implementation
    
    * Update tests
    
    * Avoid unnecessary copying

--- ballet/eng/misc.py
+++ ballet/eng/misc.py
@@ -1,16 +1,63 @@
 import funcy
 import numpy as np
 import pandas as pd
+from scipy.special import boxcox1p
+from scipy.stats import skew
+from sklearn.utils.validation import check_is_fitted
 
 from ballet.eng.base import BaseTransformer, SimpleFunctionTransformer
 from ballet.util import get_arr_desc
 
-__all__ = ['IdentityTransformer', 'ValueReplacer', 'NamedFramer']
+__all__ = [
+    'IdentityTransformer',
+    'BoxCoxTransformer',
+    'ValueReplacer',
+    'NamedFramer']
 
 
 class IdentityTransformer(SimpleFunctionTransformer):
     def __init__(self):
         super().__init__(funcy.identity)
+
+
+class BoxCoxTransformer(BaseTransformer):
+    def __init__(self, threshold, lmbda=0):
+        super().__init__()
+        self.threshold = threshold
+        self.lmbda = lmbda
+
+    def fit(self, X, y=None, **fit_args):
+        # features_to_transform_ is a bool or array[bool]
+        self.features_to_transform_ = abs(skew(X)) > self.threshold
+        return self
+
+    def transform(self, X, **transform_args):
+        check_is_fitted(self, 'features_to_transform_')
+
+        if isinstance(X, pd.DataFrame):
+            X = X.copy()
+            X.loc[:, self.features_to_transform_] = boxcox1p(
+                X.loc[:, self.features_to_transform_], self.lmbda)
+            return X
+        elif np.ndim(X) == 1:
+            return boxcox1p(
+                X, self.lmbda) if self.features_to_transform_ else X
+        elif isinstance(X, np.ndarray):
+            if self.features_to_transform_.any():
+                X = X.copy()
+                X = X.astype('float')
+                mask = np.tile(self.features_to_transform_, (X.shape[0], 1))
+                np.putmask(X, mask, boxcox1p(
+                    X[:, self.features_to_transform_], self.lmbda))
+            return X
+        elif not self.features_to_transform_:
+            # if we wouldn't otherwise have known what to do, we can pass
+            # through X if transformation was not necessary anyways
+            return X
+        else:
+            raise TypeError(
+                "Couldn't use boxcox transform on features in {}."
+                .format(get_arr_desc(X)))
 
 
 class ValueReplacer(BaseTransformer):
