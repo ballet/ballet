5694ab7c30f3290d4a1c2d5755cd431e5c76fe1a
Kelvin Lu <kelvinlu@mit.edu>
2019-02-12 15:40:32 -0500

    address comments, no testing yet

--- ballet/update.py
+++ ballet/update.py
@@ -5,56 +5,43 @@ import yaml
 
 from cookiecutter.main import cookiecutter
 
-from ballet.compat import pathlib
+from ballet.compat import pathlib, safepath
+from ballet.util.log import logger
+from ballet.project import get_config_paths, find_configs
 from ballet.quickstart import generate_project
-
-YML_FILE = 'ballet.yml'
-
-def _find_ballet_dir(path):
-    '''
-    Searches for the directory containing the ballet path,
-    using the ballet.yml as a guide
-    '''
-    if os.path.exists(path / YML_FILE):
-        return path
-    for parent in path.parents:
-        parent_path = pathlib.Path(parent)
-        if os.path.exists( parent_path / YML_FILE):
-            return parent_path
-        else:
-            continue
-    return None
 
 def _create_replay(tempdir, name):
     generate_project(replay=True, output_dir=tempdir)
     return pathlib.Path(tempdir) / name
 
 def update_project():
-    path = _find_ballet_dir(pathlib.Path(os.getcwd()))
-    yml_file = open(path / YML_FILE)
-    ballet_yml = yaml.load(yml_file)
+    cwd = pathlib.Path(os.getcwd())
+    ballet_yml = find_configs(cwd)
     try:
-        current_repo = git.Repo(path, search_parent_directories=True) # for right now
-    except Exception as e: print(e)
-
-    _tempdir = tempfile.TemporaryDirectory()
-    tempdir = _tempdir.name
-    updated_project = _create_replay(tempdir, ballet_yml['problem']['name'])
-    updated_repo = git.Repo(str(updated_project))
-    try:
-        remote_name = updated_project.parts[-1]
-        updated_remote = current_repo.create_remote(remote_name, updated_repo.working_tree_dir)
-        updated_remote.fetch()
-        current_repo.git.merge(
-            remote_name + '/master',
-            allow_unrelated_histories=True,
-            strategy_option='theirs',
-            squash=True,
-        )
-    except Exception as e: 
+        current_repo = git.Repo(safepath(cwd), search_parent_directories=True) # for right now
+    except Exception as e:
         print(e)
-    finally:
-        current_repo.delete_remote(remote_name)
+
+    with tempfile.TemporaryDirectory() as _tempdir:
+        tempdir = _tempdir.name
+        updated_project = _create_replay(tempdir, ballet_yml[0]['problem']['name'])
+        updated_repo = git.Repo(safepath(updated_project))
+        remote_name = updated_project.parts[-1]
+        try:
+            # add some randomness in the remote name
+            updated_remote = current_repo.create_remote(remote_name, updated_repo.working_tree_dir)
+            updated_remote.fetch()
+            current_repo.git.merge(
+                remote_name + '/master',
+                allow_unrelated_histories=True,
+                # strategy_option='theirs',
+                squash=True,
+            )
+        except Exception as e: 
+            print(e)
+            logger.exception('Could not complete update, merge failed')
+        finally:
+            current_repo.delete_remote(remote_name)
 
 
 def main():
