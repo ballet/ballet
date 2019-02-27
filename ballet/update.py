6cfb82a3ac51e2907d00435beb0979cca5b8c6a0
Micah Smith <micahjsmith@gmail.com>
2019-02-27 15:19:16 -0500

    Catch specific errors and re-raise

--- ballet/update.py
+++ ballet/update.py
@@ -5,6 +5,7 @@ import tempfile
 import funcy
 import git
 from cookiecutter.prompt import prompt_for_config
+from git import GitCommandError
 
 from ballet import __version__ as version
 from ballet.compat import pathlib, safepath
@@ -33,6 +34,7 @@ def _create_replay(cwd, tempdir):
     context = _get_full_context(cwd)
     slug = context['cookiecutter']['project_slug']
     old_context = None
+
     try:
         if REPLAY_PATH.exists():
             with REPLAY_PATH.open('r') as f:
@@ -44,15 +46,17 @@ def _create_replay(cwd, tempdir):
         generate_project(
             replay=True,
             output_dir=safepath(tempdir))
-    except BaseException:
+    except Exception:
         # we're missing keys, figure out which and prompt
         logger.exception(
             'Could not create a updated project copy, update failed.')
+        raise
     finally:
         # put back the old replay, if it's been overwritten
         if old_context is not None:
             with REPLAY_PATH.open('w') as f:
                 json.dump(old_context, f)
+
     return tempdir / slug
 
 
@@ -80,15 +84,17 @@ def update_project_template(create_merge_commit=False):
         current_repo = git.Repo(
             safepath(cwd),
             search_parent_directories=True)  # for right now
-    except BaseException:
-        msg = 'Could not find ballet repo, update failed'
-        logger.exception(msg)
+    except GitCommandError:
+        logger.exception('Could not find ballet repo, update failed')
+        raise
 
     with tempfile.TemporaryDirectory() as tempdir:
         updated_template = _create_replay(cwd, tempdir)
         updated_repo = git.Repo(safepath(updated_template))
+
         # add some randomness in the remote name by using tempdir
         remote_name = updated_template.parts[-1]
+
         try:
             current_repo.heads[TEMPLATE_BRANCH].checkout()
             updated_remote = current_repo.create_remote(
@@ -102,10 +108,11 @@ def update_project_template(create_merge_commit=False):
             )
             current_repo.index.commit(
                 _make_master_branch_merge_commit_message())
-        except BaseException:
-            msg = ('Could not merge changes into template-update branch, '
-                   'update failed')
-            logger.exception(msg)
+        except GitCommandError:
+            logger.exception(
+                'Could not merge changes into template-update branch, '
+                'update failed')
+            raise
         finally:
             if current_repo.active_branch != current_repo.heads.master:
                 current_repo.heads.master.checkout()
@@ -116,18 +123,21 @@ def update_project_template(create_merge_commit=False):
             TEMPLATE_BRANCH,
             squash=True,
         )
-        if not create_merge_commit:
-            commit_prompt = ('Would you like ballet to create a merge commit '
-                             'automatically? [y/N]: ')
-            answer = input(commit_prompt)
-            if 'y' in answer.lower():
-                create_merge_commit = True
-        if create_merge_commit:
-            current_repo.index.commit(
-                _make_master_branch_merge_commit_message())
-    except BaseException:
-        msg = 'Could not merge changes into master, update failed'
-        logger.exception(msg)
+    except GitCommandError:
+        logger.exception(
+            'Could not merge changes into master, update failed')
+        raise
+
+    if not create_merge_commit:
+        commit_prompt = ('Would you like ballet to create a merge commit '
+                         'automatically? [y/N]: ')
+        answer = input(commit_prompt)
+        if 'y' in answer.lower():
+            create_merge_commit = True
+
+    if create_merge_commit:
+        current_repo.index.commit(
+            _make_master_branch_merge_commit_message())
 
 
 def main():
