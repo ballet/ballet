2b1abef05bf0a8c53a4554bc287455878b122cc6
Micah Smith <micahjsmith@gmail.com>
2019-03-01 14:32:22 -0500

    Filter annoying sklearn warnings

--- ballet/__init__.py
+++ ballet/__init__.py
@@ -2,22 +2,27 @@
 
 """Top-level package for ballet."""
 
-__author__ = """Micah Smith"""
+__author__ = 'Micah Smith'
 __email__ = 'micahs@mit.edu'
 __version__ = '0.5.1-dev'
-
-
-# re-export some names
-from ballet.feature import *  # noqa
-from ballet.contrib import *  # noqa
-
-
-# configure module-level logging
-import logging  # noqa E402
-from ballet.util.log import logger  # noqa E402
-logger.addHandler(logging.NullHandler())
 
 # filter warnings
 import warnings  # noqa E402
 warnings.filterwarnings(
-    action="ignore", module="scipy", message="^internal gelsd")
+    action='ignore', module='scipy', message='^internal gelsd')
+
+# silence sklearn deprecation warnings
+import logging  # noqa E402
+logging.captureWarnings(True)
+import sklearn  # noqa
+logging.captureWarnings(False)
+warnings.filterwarnings(
+    action='ignore', module='sklearn', category=DeprecationWarning)
+
+# configure module-level logging
+from ballet.util.log import logger  # noqa E402
+logger.addHandler(logging.NullHandler())
+
+# re-export some names
+from ballet.feature import *  # noqa
+from ballet.contrib import *  # noqa
