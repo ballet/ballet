1beeabc63b945ffa68f48bbcca1cbccec867a618
Kelvin Lu <kelvinlu@mit.edu>
2019-03-13 14:02:30 -0400

    address some comments

--- ballet/validation/gfssf_validator.py
+++ ballet/validation/gfssf_validator.py
@@ -155,8 +155,23 @@ def _concat_datasets(dfs_by_src, n_samples, omit=None):
     return asarray2d(np.concatenate(filtered_dfs, axis=1))
 
 
+def _compute_lmbdas(unnorm_lmbda_1, unnorm_lmbda_2, feature, acc_by_src):
+    feat_srcs = acc_by_src.keys()
+    num_features = 1 + len(feat_srcs)
+    num_feature_cols = feature.shape[1]
+    for acc_feat_src in feat_srcs:
+        num_feature_cols += acc_by_src[acc_feat_src].shape[1]
+    return (unnorm_lmbda_1 / num_features, unnorm_lmbda_2 / num_feature_cols)
+
+
+def _compute_threshold(lmbda_1, lmbda_2, feat_df, omit_df):
+    _, n_features = feat_df.shape
+    _, n_features_omitted = omit_df.shape
+    return lmbda_1 + lmbda_2 * (n_features - n_features_omitted)
+
+
 class GFSSFAcceptanceEvaluator(FeatureAcceptanceEvaluator):
-    def __init__(self, X_df, y, features, lmbda_1=0, lmbda_2=0):
+    def __init__(self, X_df, y, features, lmbda_1=0., lmbda_2=0.):
         super().__init__(X_df, y, features)
         self.y = asarray2d(y)
         if (lmbda_1 <= 0):
@@ -167,21 +182,16 @@ class GFSSFAcceptanceEvaluator(FeatureAcceptanceEvaluator):
         self.lmbda_2 = lmbda_2
 
     def judge(self, feature):
-        feature_df = make_mapper([feature]).fit_transform(self.X_df, self.y)
-        n_samples, n_feature_clms = feature_df.shape
-        n_feature_clms_arr = n_feature_clms
-        n_feature_grps_arr = 1
+        feature_df = feature.as_dataframe_mapper().fit_transform(self.X_df, self.y)
+        n_samples, n_feature_cols = feature_df.shape
         feature_dfs_by_src = {}
         for accepted_feature in self.features:
-            accepted_df = make_mapper(
-                [feature]).fit_transform(
+            accepted_df = accepted_feature.as_dataframe_mapper().fit_transform(
                 self.X_df, self.y)
-            n_feature_clms_arr += accepted_df.shape[1]
-            n_feature_grps_arr += 1
             feature_dfs_by_src[accepted_feature.source] = accepted_df
 
-        lmbda_1 = self.lmbda_1 / n_feature_grps_arr
-        lmbda_2 = self.lmbda_2 / n_feature_clms_arr
+        lmbda_1, lmbda_2 = _compute_lmbdas(self.lmbda_1, self.lmbda_2, feature, feature_dfs_by_src)
+
         logger.info(
             'Judging Feature using GFSSF: lambda_1={l1}, lambda_2={l2}'.format(
                 l1=lmbda_1, l2=lmbda_2))
@@ -195,16 +205,15 @@ class GFSSFAcceptanceEvaluator(FeatureAcceptanceEvaluator):
             logger.debug(
                 'Conditional Mutual Information Score: {}'.format(cmi))
             cmi_omit = 0
-            n_clms_omit = 0
+            n_omit_cols = 0
             if omit is not '':
                 omit_df = feature_dfs_by_src[omit]
+                _, n_omit_cols = omit_df.shape
                 cmi_omit = _estimate_conditional_information(
                     omit_df, self.y, z)
-                _, n_clms_omit = omit_df.shape
                 logger.debug('Omitted CMI Score: {}'.format(cmi_omit))
             statistic = cmi - cmi_omit
-            threshold = lmbda_1 + \
-                lmbda_2 * (n_feature_clms - n_clms_omit)
+            threshold = _compute_threshold(lmbda_1, lmbda_2, n_feature_cols, n_omit_cols)
             logger.debug('Calculated Threshold: {}'.format(threshold))
             if statistic >= threshold:
                 logger.debug(
