25d155269d84aa0159c7ee81e888dd63466d6752
Micah Smith <micahjsmith@gmail.com>
2019-03-25 15:27:38 -0400

    Simplify invocation of cookiecutter in templating

--- ballet/templating.py
+++ ballet/templating.py
@@ -1,35 +1,45 @@
-from cookiecutter.main import cookiecutter
+from cookiecutter.main import cookiecutter as _cookiecutter
+from funcy import re_test, walk, walk_values, wraps
 
 from ballet.compat import pathlib, safepath
+from ballet.util.fs import ispathlike, synctree
 
 TEMPLATES_PATH = pathlib.Path(__file__).resolve().parent.joinpath('templates')
 FEATURE_TEMPLATE_PATH = TEMPLATES_PATH.joinpath('feature_template')
 PROJECT_TEMPLATE_PATH = TEMPLATES_PATH.joinpath('project_template')
 
 
-def _get_project_template_path():
-    return safepath(PROJECT_TEMPLATE_PATH)
+def _stringify_path(obj):
+    return str(obj) if ispathlike(obj) else obj
 
 
+@wraps(_cookiecutter)
+def cookiecutter(*args, **kwargs):
+    args = walk(_stringify_path, args)
+    kwargs = walk_values(_stringify_path, kwargs)
+    return _cookiecutter(*args, **kwargs)
 
-def render_project_template(**kwargs):
+
+def render_project_template(**cc_kwargs):
     """Generate a ballet project according to the project template
 
     Args:
-        **kwargs: options for the cookiecutter template
+        **cc_kwargs: options for the cookiecutter template
     """
-    # must use str because cookiecutter can't handle path-like objects.
-    project_template_path = str(_get_project_template_path())
-    if 'output_dir' in kwargs:
-        kwargs['output_dir'] = str(kwargs['output_dir'])
-    return cookiecutter(project_template_path, **kwargs)
+    project_template_path = PROJECT_TEMPLATE_PATH
+    return cookiecutter(project_template_path, **cc_kwargs)
 
 
-def render_feature_template(**kwargs):
+def render_feature_template(**cc_kwargs):
     """Create a stub for a new feature
 
     Args:
-        **kwargs: options for the cookiecutter template
+        **cc_kwargs: options for the cookiecutter template
+    """
+    feature_template_path = FEATURE_TEMPLATE_PATH
+    return cookiecutter(feature_template_path, **cc_kwargs)
+
+
     """
     # must use str because cookiecutter can't handle path-like objects.
     feature_template_path = str(FEATURE_TEMPLATE_PATH)
