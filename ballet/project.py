309930aa5a7504b8d4eba0773da7453aa28b4f34
Kelvin Lu <kelvinlu@mit.edu>
2019-04-08 14:55:06 -0400

    requested refactor of merge

--- ballet/project.py
+++ ballet/project.py
@@ -7,7 +7,7 @@ from funcy import get_in, memoize, partial
 from ballet.compat import pathlib
 from ballet.exc import ConfigurationError
 from ballet.util.ci import get_travis_branch, get_travis_pr_num
-from ballet.util.git import get_branch, get_pr_num
+from ballet.util.git import get_branch, get_pr_num, is_merge_commit
 from ballet.util.mod import import_module_at_path
 
 DEFAULT_CONFIG_NAME = 'ballet.yml'
@@ -213,7 +213,7 @@ class Project:
         2. The project repo's head commit is a merge commit.
         """
 
-        return self.on_master() and len(self.repo.head.commit.parents) > 1
+        return self.on_master() and is_merge_commit(self.repo.head.commit)
 
     @property
     def path(self):
