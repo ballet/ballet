41dd1f13a5cc30f334ad4bb438a0ea0696619afb
Micah Smith <micahjsmith@gmail.com>
2019-05-06 01:13:18 +0100

    Add ConditionalTransformer
    Adapt BoxCoxTransformer to be one type of conditional transformer that
    tests for skew and applies boxcox

--- ballet/eng/misc.py
+++ ballet/eng/misc.py
@@ -3,16 +3,17 @@ import numpy as np
 import pandas as pd
 from scipy.special import boxcox1p
 from scipy.stats import skew
-from sklearn.utils.validation import check_is_fitted
 
-from ballet.eng.base import BaseTransformer, SimpleFunctionTransformer
+from ballet.eng.base import (
+    BaseTransformer, ConditionalTransformer, SimpleFunctionTransformer)
 from ballet.util import get_arr_desc
 
 __all__ = [
     'IdentityTransformer',
     'BoxCoxTransformer',
     'ValueReplacer',
-    'NamedFramer']
+    'NamedFramer',
+]
 
 
 class IdentityTransformer(SimpleFunctionTransformer):
@@ -22,7 +23,7 @@ class IdentityTransformer(SimpleFunctionTransformer):
         super().__init__(funcy.identity)
 
 
-class BoxCoxTransformer(BaseTransformer):
+class BoxCoxTransformer(ConditionalTransformer):
     """Conditionally apply the Box-Cox transformation
 
     In the fit stage, determines which variables (columns) have absolute skew
@@ -36,44 +37,14 @@ class BoxCoxTransformer(BaseTransformer):
     See also:
         https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.boxcox1p.html
     """
-
     def __init__(self, threshold, lmbda=0):
-        super().__init__()
-        self.threshold = threshold
-        self.lmbda = lmbda
+        def condition(X):
+            return abs(skew(X)) > threshold
 
-    def fit(self, X, y=None, **fit_args):
-        # features_to_transform_ is a bool or array[bool]
-        self.features_to_transform_ = abs(skew(X)) > self.threshold
-        return self
+        def transform(X):
+            return boxcox1p(X, lmbda)
 
-    def transform(self, X, **transform_args):
-        check_is_fitted(self, 'features_to_transform_')
-
-        if isinstance(X, pd.DataFrame):
-            X = X.copy()
-            X.loc[:, self.features_to_transform_] = boxcox1p(
-                X.loc[:, self.features_to_transform_], self.lmbda)
-            return X
-        elif np.ndim(X) == 1:
-            return boxcox1p(
-                X, self.lmbda) if self.features_to_transform_ else X
-        elif isinstance(X, np.ndarray):
-            if self.features_to_transform_.any():
-                X = X.copy()
-                X = X.astype('float')
-                mask = np.tile(self.features_to_transform_, (X.shape[0], 1))
-                np.putmask(X, mask, boxcox1p(
-                    X[:, self.features_to_transform_], self.lmbda))
-            return X
-        elif not self.features_to_transform_:
-            # if we wouldn't otherwise have known what to do, we can pass
-            # through X if transformation was not necessary anyways
-            return X
-        else:
-            raise TypeError(
-                "Couldn't use Box-Cox transform on features in {}."
-                .format(get_arr_desc(X)))
+        super().__init__(condition, transform)
 
 
 class ValueReplacer(BaseTransformer):
