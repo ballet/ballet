39c94fada0aeed6724bbc90ef993f960ffa34fd8
Micah Smith <micahjsmith@gmail.com>
2019-11-11 17:12:34 -0500

    mark slow test

--- tests/end_to_end/test_quickstart_end_to_end.py
+++ tests/end_to_end/test_quickstart_end_to_end.py
@@ -1,8 +1,10 @@
+import pytest
 from cookiecutter.utils import work_in
 
 from ballet.compat import safepath
 
 
+@pytest.mark.slow
 def test_quickstart_install(quickstart, virtualenv):
     d = quickstart.tempdir.joinpath(quickstart.project_slug).absolute()
     with work_in(safepath(d)):
