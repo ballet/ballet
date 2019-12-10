c5b9b810bb19a54633d24a8aba9adc358a82a1ea
Micah Smith <micahjsmith@gmail.com>
2019-12-10 16:25:21 -0500

    Add py38 compatibility

--- setup.py
+++ setup.py
@@ -78,6 +78,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description='Core functionality for lightweight, collaborative data science projects',
     entry_points = {
