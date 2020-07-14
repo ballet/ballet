31981b9e77790bac5fa26d0104bf5ae15a1d0524
Micah Smith <micahjsmith@gmail.com>
2020-07-14 19:13:56 -0400

    Update project template setup with pyinvoke etc.

---
+++ ballet/templates/project_template/{{cookiecutter.project_slug}}/tasks.py
@@ -0,0 +1,26 @@
+from invoke import task
+
+
+@task
+def install(c):
+    """install the package to the active Python's site-packages"""
+    c.run('pip install -e .')
+
+
+def rmdir(c, name):
+    c.run('rm -rf {name}')
+
+
+def rm(c, pattern):
+    c.run(f'find . -name \'{pattern}\' -exec rm -rf {{}} +')
+
+
+@task
+def clean(c):
+    """Clean python artifacts"""
+    for name in ['build', 'dist', '.eggs']:
+        rmdir(c, name)
+
+    for pattern in ['*.egg-info', '*.egg', '*.pyc', '*.pyo', '*~',
+                    '__pycache__']:
+        rm(c, pattern)
