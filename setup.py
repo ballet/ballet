2cce259117d55f7a7faa4093dde206a375f1106e
Micah Smith <micahjsmith@gmail.com>
2020-07-21 11:48:17 -0400

    Bump version: 0.7.1 → 0.7.2

--- setup.py
+++ setup.py
@@ -100,6 +100,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/HDI-Project/ballet',
-    version='0.7.1',
+    version='0.7.2',
     zip_safe=False,
 )
