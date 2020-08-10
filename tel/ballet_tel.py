ec45ba525c8851fbb4301d2b3e98889a469d8245
Micah Smith <micahjsmith@gmail.com>
2020-08-10 14:23:12 -0400

    update

--- tel/ballet_tel.py
+++ tel/ballet_tel.py
@@ -1,5 +1,5 @@
 import requests
-from funcy import decorator, silent
+from funcy import contextmanager, decorator, silent
 from notebook.notebookapp import list_running_servers
 
 servers = list(list_running_servers())
@@ -7,22 +7,53 @@ server = servers[0] if len(servers) == 1 else None
 
 import ballet.client  # noqa
 
+call_depth = 0
+
 
 @silent
 def event(name, details):
-    if server is not None:
-        requests.post(
-            server['url'] + 'ballet/tel',
-            headers={'token': server['token']},
-            json={'name': name, 'details': details})
+    if call_depth == 0:
+        if server is not None:
+            requests.post(
+                server['url'] + 'ballet/tel',
+                headers={'token': server['token']},
+                json={'name': name, 'details': details})
+
+
+@contextmanager
+def disable_events():
+    global call_depth
+    call_depth += 1
+    try:
+        yield
+    finally:
+        call_depth -= 1
 
 
 @decorator
 def instrument(call):
     event_name = call._func.__qualname__
-    result = call()
-    event(event_name, {'result': result})
-    return result
+
+    try:
+        with disable_events():
+            result = call()
+        event(event_name, {'result': result, 'error': False})
+        return result
+    except Exception:
+        event(event_name, {'result': None, 'error': True})
+        raise
+
+
+def instrumentproperty(oldprop):
+
+    @property
+    def prop(self):
+        event_name = oldprop.fget.__qualname__
+        event(event_name, {})
+        with disable_events():
+            return oldprop.fget(self)
+
+    return prop
 
 
 def install():
@@ -30,5 +61,5 @@ def install():
         ballet.client.Client.validate_feature_api)
     ballet.client.Client.validate_feature_acceptance = instrument(
         ballet.client.Client.validate_feature_acceptance)
-    ballet.client.Client.api = instrument(
+    ballet.client.Client.api = instrumentproperty(
         ballet.client.Client.api)
