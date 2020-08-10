4f2344f936d425c30d756fa244900fc77eae2edf
Micah Smith <micahjsmith@gmail.com>
2020-08-10 11:44:24 -0400

    add initial tel

---
+++ tel/ballet_tel.py
@@ -0,0 +1,2 @@
+def install():
+    print('installed')
---
+++ tel/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    name='ballet-tel',
+    install_requires=['ballet'],
+    py_modules=['ballet_tel'],
+)
