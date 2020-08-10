d1611d27de21d3f2c3218bfa2274d312ba879ad5
Micah Smith <micahjsmith@gmail.com>
2020-08-10 13:17:33 -0400

    update

--- tel/ballet_tel.py
+++ tel/ballet_tel.py
@@ -1,2 +1,32 @@
+import ballet.client
+import requests
+
+from funcy import decorator
+from notebook.notebookapp import list_running_servers
+servers = list(list_running_servers())
+server = servers[0] if len(servers) == 1 else None
+
+
+def event(name, details):
+    if server is not None:
+        requests.post(
+            server['url'] + 'ballet/tel',
+            headers={'token': server['token']},
+            json={'name': name, 'details': details})
+
+
+@decorator
+def instrument(call):
+    event_name = call._func.__qualname__
+    result = call()
+    event(event_name, {'result': result})
+    return result
+
+
 def install():
-    print('installed')
+    ballet.client.Client.validate_feature_api = instrument(
+        ballet.client.Client.validate_feature_api)
+    ballet.client.Client.validate_feature_acceptance = instrument(
+        ballet.client.Client.validate_feature_acceptance)
+    ballet.client.Client.api = instrument(
+        ballet.client.Client.api)
