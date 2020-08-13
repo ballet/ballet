541f16a73478756217b2c455462729cb61389426
Micah Smith <micahjsmith@gmail.com>
2020-08-13 00:35:17 -0400

    Bump version: 0.7.7 → 0.7.8

--- ballet/__init__.py
+++ ballet/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'Micah Smith'
 __email__ = 'micahs@mit.edu'
-__version__ = '0.7.7'
+__version__ = '0.7.8'
 
 # filter warnings
 import warnings  # noqa E402
--- setup.py
+++ setup.py
@@ -112,6 +112,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/HDI-Project/ballet',
-    version='0.7.7',
+    version='0.7.8',
     zip_safe=False,
 )
