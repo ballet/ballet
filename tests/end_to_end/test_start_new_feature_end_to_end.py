8de34410a7e5d4448baec39ce4348320fb39a805
Micah Smith <micahjsmith@gmail.com>
2021-02-16 16:14:39 -0500

    Add test for branching functionality

--- tests/end_to_end/test_start_new_feature_end_to_end.py
+++ tests/end_to_end/test_start_new_feature_end_to_end.py
@@ -1,10 +1,17 @@
 from unittest.mock import patch
 
+import pytest
+
 from ballet.templating import start_new_feature
 from ballet.util import work_in
 
 
-def test_start_new_feature(quickstart, caplog):
+@pytest.mark.parametrize(
+    'branching',
+    [True, False],
+)
+def test_start_new_feature(quickstart, caplog, branching):
+    default_branch = quickstart.project.branch
     with work_in(quickstart.project.path):
         with patch('ballet.project.Project.from_cwd') as mock_from_cwd:
             mock_from_cwd.return_value = quickstart.project
@@ -12,6 +19,13 @@ def test_start_new_feature(quickstart, caplog):
                 'username': 'username',
                 'featurename': 'featurename',
             }
-            start_new_feature(no_input=True, extra_context=extra_context)
+            start_new_feature(
+                branching=branching, no_input=True, extra_context=extra_context
+            )
 
     assert 'Start new feature successful' in caplog.text
+
+    if branching:
+        assert quickstart.project.branch == 'username/feature-featurename'
+    else:
+        assert quickstart.project.branch == default_branch
