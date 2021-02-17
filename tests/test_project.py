6372b94f77dc3e468f2764d4390ca9a880d6e0d7
Micah Smith <micahjsmith@gmail.com>
2021-02-16 21:57:48 -0500

    Migrate test_project to pytest

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,43 +1,45 @@
-import unittest
 from unittest.mock import ANY, PropertyMock, patch
+
+import pytest
 
 from ballet.project import Project, detect_github_username
 
 
-class ProjectTest(unittest.TestCase):
+@pytest.mark.xfail
+def test_load_config_at_path():
+    raise NotImplementedError
 
-    @unittest.expectedFailure
-    def test_load_config_at_path(self):
-        raise NotImplementedError
 
-    @unittest.expectedFailure
-    def test_load_config_in_dir(self):
-        raise NotImplementedError
+@pytest.mark.xfail
+def test_load_config_in_dir():
+    raise NotImplementedError
 
-    @patch('ballet.project.Project.repo', new_callable=PropertyMock)
-    def test_detect_github_username_config(self, mock_project_repo):
-        expected_username = 'Foo Bar'
 
-        # output of project.repo.config_reader().get_value(...)
-        mock_get_value = (mock_project_repo
-                          .return_value
-                          .config_reader
-                          .return_value
-                          .get_value)
-        mock_get_value.return_value = expected_username
+@patch('ballet.project.Project.repo', new_callable=PropertyMock)
+def test_detect_github_username_config(mock_project_repo):
+    expected_username = 'Foo Bar'
 
-        project = Project(None)
-        username = detect_github_username(project)
-        self.assertEqual(expected_username, username)
+    # output of project.repo.config_reader().get_value(...)
+    mock_get_value = (mock_project_repo
+                        .return_value
+                        .config_reader
+                        .return_value
+                        .get_value)
+    mock_get_value.return_value = expected_username
 
-        mock_get_value.assert_called_with('github', 'user', default=ANY)
+    project = Project(None)
+    username = detect_github_username(project)
+    assert expected_username == username
 
-    @patch('ballet.project.Project.repo', new_callable=PropertyMock)
-    @patch('ballet.project.get_pr_num')
-    def test_project_pr_num(self, mock_get_pr_num, mock_repo):
-        expected = 3
-        mock_get_pr_num.return_value = expected
+    mock_get_value.assert_called_with('github', 'user', default=ANY)
 
-        package = None
-        project = Project(package)
-        self.assertEqual(project.pr_num, expected)
+
+@patch('ballet.project.Project.repo', new_callable=PropertyMock)
+@patch('ballet.project.get_pr_num')
+def test_project_pr_num(mock_get_pr_num, mock_repo):
+    expected = 3
+    mock_get_pr_num.return_value = expected
+
+    package = None
+    project = Project(package)
+    assert project.pr_num == expected
