3479f438e71650f3f7fccb64ce83d166230c9a48
Micah Smith <micahjsmith@gmail.com>
2021-03-02 20:26:55 -0500

    break out test_log

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,66 +1,10 @@
-import logging
-import random
 import sys
 import unittest
 
-import pytest
-
-import ballet
-import ballet.util
-import ballet.util.ci
-import ballet.util.fs
-import ballet.util.io
-from ballet.util import one_or_raise
 from ballet.util.code import blacken_code, get_source, is_valid_python
 from ballet.util.mod import (  # noqa F401
     import_module_at_path, import_module_from_modname,
     import_module_from_relpath, modname_to_relpath, relpath_to_modname,)
-
-
-class LogTest(unittest.TestCase):
-
-    def setUp(self):
-        self.name = str(random.randint(0, 1 << 10))
-        self.logger = logging.getLogger(self.name)
-
-    def test_enable(self):
-        for level in [logging.INFO, 'CRITICAL']:
-            with self.assertLogs(self.logger, level) as cm:
-                ballet.util.log.enable(self.logger, level, echo=True)
-            msg = one_or_raise(cm.output)
-            assert 'enabled' in msg
-
-    def test_level_filter_matches(self):
-        ballet.util.log.enable(self.logger, level='DEBUG', echo=False)
-        self.logger.addFilter(
-            ballet.util.log.LevelFilter(logging.CRITICAL))
-
-        # does log message at level CRITICAL
-        with self.assertLogs(self.logger, logging.CRITICAL):
-            self.logger.critical('msg')
-
-    def test_level_filter_not_matches(self):
-        ballet.util.log.enable(self.logger, level='DEBUG', echo=False)
-        self.logger.addFilter(
-            ballet.util.log.LevelFilter(logging.DEBUG))
-
-        # does *not* log message at level INFO > DEBUG
-        with pytest.raises(AssertionError):
-            with self.assertLogs(self.logger, logging.INFO):
-                self.logger.info('msg')
-
-    @unittest.expectedFailure
-    def test_logging_context(self):
-        ballet.util.log.enable(self.logger, level='DEBUG', echo=False)
-        with self.assertLogs(self.logger, logging.DEBUG):
-            self.logger.debug('msg')
-
-        # TODO not sure why this fails - think the unittest cm is doing
-        # something weird
-        with ballet.util.log.LoggingContext(self.logger, level='INFO'):
-            with pytest.raises(AssertionError):
-                with self.assertLogs(self.logger, logging.DEBUG):
-                    self.logger.debug('msg')
 
 
 class CodeTest(unittest.TestCase):
---
+++ tests/util/test_log.py
@@ -0,0 +1,53 @@
+import logging
+import random
+
+import pytest
+
+from ballet.util.log import LevelFilter, LoggingContext, enable
+
+
+@pytest.fixture
+def logger():
+    name = str(random.randint(0, 1 << 10))
+    return logging.getLogger(name)
+
+
+@pytest.mark.parametrize('level', [logging.INFO, 'CRITICAL'])
+def test_enable(logger, caplog, level):
+    with caplog.at_level(level, logger=logger.name):
+        enable(logger, level, echo=True)
+    assert 'enabled' in caplog.text
+
+
+def test_level_filter_matches(logger, caplog):
+    enable(logger, level='DEBUG', echo=False)
+    logger.addFilter(LevelFilter(logging.CRITICAL))
+
+    # does log message at level CRITICAL
+    with caplog.at_level(logging.CRITICAL, logger=logger.name):
+        logger.critical('msg')
+
+    assert caplog.text
+
+
+def test_level_filter_not_matches(logger, caplog):
+    enable(logger, level='DEBUG', echo=False)
+    logger.addFilter(
+        LevelFilter(logging.DEBUG))
+
+    # does *not* log message at level INFO > DEBUG
+    with caplog.at_level(logging.INFO, logger=logger.name):
+        logger.info('msg')
+
+    assert not caplog.text
+
+
+@pytest.mark.xfail
+def test_logging_context(logger, caplog):
+    # TODO not sure why this fails - think the unittest cm is doing
+    # something weird
+    enable(logger, level='DEBUG', echo=False)
+    with LoggingContext(logger, level='INFO'):
+        with caplog.at_level(logging.DEBUG, logger=logger.name):
+            logger.debug('msg')
+    assert not caplog.text
