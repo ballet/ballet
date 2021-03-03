71363c86d1f1131579832214a3cbd548f17bbb94
Micah Smith <micahjsmith@gmail.com>
2021-03-03 09:30:35 -0500

    rewrite test_external

--- tests/eng/test_external.py
+++ tests/eng/test_external.py
@@ -1,48 +1,54 @@
-import unittest
+"""
+If the extra is not installed, then the test passes. If the extra is
+installed, we should be able to import the last attribute exported from that
+module.
+"""
 
 
-class ExternalTest(unittest.TestCase):
+def test_category_encoders():
+    try:
+        import category_encoders  # noqa F401
+    except ImportError:
+        pass
+    else:
+        from ballet.eng.category_encoders import WOEEncoder  # noqa F401
 
-    def test_category_encoders(self):
-        try:
-            import category_encoders  # noqa F401
-        except ImportError:
-            pass
-        else:
-            from ballet.eng.category_encoders import WOEEncoder  # noqa F401
 
-    def test_feature_engine(self):
-        try:
-            import feature_engine  # noqa F401
-        except ImportError:
-            pass
-        else:
-            from ballet.eng.feature_engine import (  # noqa F401
-                YeoJohnsonTransformer,)
+def test_feature_engine():
+    try:
+        import feature_engine  # noqa F401
+    except ImportError:
+        pass
+    else:
+        from ballet.eng.feature_engine import YeoJohnsonTransformer  # noqa f401
 
-    def test_featuretools(self):
-        try:
-            import featuretools  # noqa F401
-        except ImportError:
-            pass
-        else:
-            from ballet.eng.featuretools import DFSTransformer  # noqa F401
 
-    def test_skits(self):
-        try:
-            import skits  # noqa F401
-        except ImportError:
-            pass
-        else:
-            from ballet.eng.skits import HorizonTransformer  # noqa F401
+def test_featuretools():
+    try:
+        import featuretools  # noqa F401
+    except ImportError:
+        pass
+    else:
+        from ballet.eng.featuretools import DFSTransformer  # noqa F401
 
-    def test_sklearn(self):
-        from ballet.eng.sklearn import KNNImputer  # noqa F401
 
-    def test_tsfresh(self):
-        try:
-            import tsfresh  # noqa F401
-        except ImportError:
-            pass
-        else:
-            from ballet.eng.tsfresh import FeatureAugmenter  # noqa F401
+def test_skits():
+    try:
+        import skits  # noqa F401
+    except ImportError:
+        pass
+    else:
+        from ballet.eng.skits import HorizonTransformer  # noqa F401
+
+
+def test_sklearn():
+    from ballet.eng.sklearn import KNNImputer  # noqa F401
+
+
+def test_tsfresh():
+    try:
+        import tsfresh  # noqa F401
+    except ImportError:
+        pass
+    else:
+        from ballet.eng.tsfresh import FeatureAugmenter  # noqa F401
