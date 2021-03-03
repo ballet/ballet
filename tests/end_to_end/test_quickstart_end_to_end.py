db3b709b8b1e648eadc31f92cd7a56f66b6611aa
Micah Smith <micahjsmith@gmail.com>
2021-03-03 11:56:20 -0500

    Use manual install command in quickstart install test

--- tests/end_to_end/test_quickstart_end_to_end.py
+++ tests/end_to_end/test_quickstart_end_to_end.py
@@ -1,36 +1,39 @@
 import re
+from pkg_resources import working_set
 
 import pytest
 
 
 @pytest.mark.slow
 def test_quickstart_install(quickstart, virtualenv):
-    """Test that we can install the project resulting from quickstart"""
+    """Test that we can install the project resulting from quickstart
+
+    Note that virtualenv.install_package is just bogus and should be avoided.
+    """
     d = quickstart.tempdir.joinpath(quickstart.project_slug).resolve()
 
-    # install development version of ballet
-    # since ballet is present in the "working set", pytest-virtualenv will
-    # run:
-    #    cd <ballet source directory> && python setup.py -q develop
-    # unfortunately this will install all ballet dependencies from the
-    # network, though they should be cached if tests are being run
-    virtualenv.install_package('ballet', build_egg=False)
+    # install ballet
+    ballet_pkg = next(p for p in working_set if p.project_name == 'ballet')
+    if ballet_pkg is None:
+        raise RuntimeError(
+            'something is not right; ballet should be installed when running'
+            'tests!')
 
-    # patch setup.py to remove ballet dependency by removing the
-    # install_requires key entirely
+    virtualenv.run(f'python -m pip install -e {ballet_pkg.location}')
+
+    # patch setup.py to unpin dependency
     with d.joinpath('setup.py').open('r') as f:
         setup_contents = f.read()
     setup_contents = re.sub(
-        re.compile(r'^\s*install_requires=.+,$', flags=re.MULTILINE),
-        '',
+        r'ballet==[0-9.+-]+',
+        'ballet',
         setup_contents,
     )
     with d.joinpath('setup.py').open('w') as f:
         f.write(setup_contents)
 
-    # install the current package
-    # can't use virtualenv.install_package, as it appears to be broken
-    virtualenv.run(f'python -m pip install -e {d}', capture=False)
+    # install the quickstart project
+    virtualenv.run(f'python -m pip install -e {d}')
 
     # should be installed now, or will fail with error code != 0
     virtualenv.run(
