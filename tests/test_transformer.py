d77af95e44451c41e131425f6daa70bfb7dbe4c0
Micah Smith <micahjsmith@gmail.com>
2021-03-02 19:31:11 -0500

    rewrite tests/test_transformer

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,5 +1,3 @@
-import unittest
-
 import funcy
 import numpy as np
 import pandas as pd
@@ -15,136 +13,110 @@ from ballet.util import asarray2d
 from .util import FragileTransformer, FragileTransformerPipeline
 
 
-class DelegatingRobustTransformerTest(unittest.TestCase):
+@pytest.fixture
+def sample_data():
+    X_ser = pd.util.testing.makeFloatSeries()
+    X_df = X_ser.to_frame()
+    X_arr1d = np.asarray(X_ser)
+    X_arr2d = np.asarray(X_df)
+    y_ser = X_ser.copy()
+    y_df = X_df.copy()
+    y_arr1d = np.asarray(y_ser)
+    y_arr2d = np.asarray(y_df)
+    d = {
+        'ser': (X_ser, y_ser),
+        'df': (X_df, y_df),
+        'arr1d': (X_arr1d, y_arr1d),
+        'arr2d': (X_arr2d, y_arr2d),
+    }
+    return d
 
-    def setUp(self):
-        self.X_ser = pd.util.testing.makeFloatSeries()
-        self.X_df = self.X_ser.to_frame()
-        self.X_arr1d = np.asarray(self.X_ser)
-        self.X_arr2d = np.asarray(self.X_df)
-        self.y_ser = self.X_ser.copy()
-        self.y_df = self.X_df.copy()
-        self.y_arr1d = np.asarray(self.y_ser)
-        self.y_arr2d = np.asarray(self.y_df)
 
-        self.d = {
-            'ser': (self.X_ser, self.y_ser),
-            'df': (self.X_df, self.y_df),
-            'arr1d': (self.X_arr1d, self.y_arr1d),
-            'arr2d': (self.X_arr2d, self.y_arr2d),
-        }
+@pytest.mark.parametrize(
+    'input_types, bad_input_checks, catches',
+    [
+        (
+            ('ser', ),
+            (lambda x: isinstance(x, pd.Series), ),
+            (ValueError, TypeError),
+        ),
+        (
+            ('ser', 'df', ),
+            (
+                lambda x: isinstance(x, pd.Series),
+                lambda x: isinstance(x, pd.DataFrame)
+            ),
+            (ValueError, TypeError),
+        ),
+        (
+            ('ser', 'df', 'arr1d'),
+            (
+                lambda x: isinstance(x, pd.Series),
+                lambda x: isinstance(x, pd.DataFrame),
+                lambda x: isinstance(x, np.ndarray) and x.ndim == 1,
+            ),
+            (ValueError, TypeError),
+        )
+    ],
+    ids=[
+        'ser',
+        'df',
+        'arr',
+    ]
+)
+@pytest.mark.parametrize(
+    'transformer_maker',
+    [
+        FragileTransformer,
+        funcy.partial(FragileTransformerPipeline, 3)
+    ]
+)
+def test_robust_transformer(
+    input_types, bad_input_checks, catches, transformer_maker,
+    sample_data,
+):
+    fragile_transformer = transformer_maker(bad_input_checks, catches)
+    robust_transformer = DelegatingRobustTransformer(
+        transformer_maker(bad_input_checks, catches))
 
-    def _test_robust_transformer(
-        self, input_types, bad_input_checks, catches,
-        transformer_maker=FragileTransformer
-    ):
-        fragile_transformer = transformer_maker(bad_input_checks, catches)
-        robust_transformer = DelegatingRobustTransformer(
-            transformer_maker(bad_input_checks, catches))
+    for input_type in input_types:
+        X, y = sample_data[input_type]
+        # fragile transformer raises error
+        with pytest.raises(catches):
+            fragile_transformer.fit_transform(X, y)
+        # robust transformer does not raise error
+        X_robust = robust_transformer.fit_transform(X, y)
+        assert np.array_equal(
+            asarray2d(X),
+            asarray2d(X_robust)
+        )
 
+
+def test_robust_transformer_sklearn(sample_data):
+    Transformers = (
+        SimpleImputer,
+        sklearn.preprocessing.StandardScaler,
+        sklearn.preprocessing.Binarizer,
+        sklearn.preprocessing.PolynomialFeatures,
+    )
+    # some of these input types are bad for sklearn.
+    input_types = ('ser', 'df', 'arr1d')
+    for Transformer in Transformers:
+        robust_transformer = DelegatingRobustTransformer(Transformer())
         for input_type in input_types:
-            X, y = self.d[input_type]
-            # fragile transformer raises error
-            with pytest.raises(catches):
-                fragile_transformer.fit_transform(X, y)
-            # robust transformer does not raise error
-            X_robust = robust_transformer.fit_transform(X, y)
-            assert np.array_equal(
-                    asarray2d(X),
-                    asarray2d(X_robust)
-                )
+            X, y = sample_data[input_type]
+            robust_transformer.fit_transform(X, y=y)
 
-    def test_robust_transformer_ser(self):
-        input_types = ('ser',)
-        bad_input_checks = (
-            lambda x: isinstance(x, pd.Series),
-        )
-        catches = (ValueError, TypeError)
-        self._test_robust_transformer(input_types, bad_input_checks, catches)
 
-    def test_robust_transformer_df(self):
-        input_types = ('ser', 'df',)
-        bad_input_checks = (
-            lambda x: isinstance(x, pd.Series),
-            lambda x: isinstance(x, pd.DataFrame),
-        )
-        catches = (ValueError, TypeError)
-        self._test_robust_transformer(input_types, bad_input_checks, catches)
-
-    def test_robust_transformer_arr(self):
-        input_types = ('ser', 'df', 'arr1d')
-        bad_input_checks = (
-            lambda x: isinstance(x, pd.Series),
-            lambda x: isinstance(x, pd.DataFrame),
-            lambda x: isinstance(x, np.ndarray) and x.ndim == 1,
-        )
-        catches = (ValueError, TypeError)
-        self._test_robust_transformer(input_types, bad_input_checks, catches)
-
-    def test_robust_transformer_sklearn(self):
-        Transformers = (
-            SimpleImputer,
-            sklearn.preprocessing.StandardScaler,
-            sklearn.preprocessing.Binarizer,
-            sklearn.preprocessing.PolynomialFeatures,
-        )
-        # some of these input types are bad for sklearn.
-        input_types = ('ser', 'df', 'arr1d')
-        for Transformer in Transformers:
-            robust_transformer = DelegatingRobustTransformer(Transformer())
-            for input_type in input_types:
-                X, y = self.d[input_type]
-                robust_transformer.fit_transform(X, y=y)
-
-    def test_robust_transformer_str_repr(self):
-        robust_transformer = DelegatingRobustTransformer(IdentityTransformer())
-        for func in [str, repr]:
-            s = func(robust_transformer)
-            assert len(s) > 0
-
-    def _test_robust_transformer_pipeline(
-        self, input_types, bad_input_checks, catches
-    ):
-        FragileTransformerPipeline3 = funcy.partial(
-            FragileTransformerPipeline, 3)
-        return self._test_robust_transformer(
-            input_types,
-            bad_input_checks,
-            catches,
-            transformer_maker=FragileTransformerPipeline3)
-
-    def test_robust_transformer_pipeline_ser(self):
-        input_types = ('ser',)
-        bad_input_checks = (
-            lambda x: isinstance(x, pd.Series),
-        )
-        catches = (ValueError, TypeError)
-        self._test_robust_transformer_pipeline(
-            input_types, bad_input_checks, catches)
-
-    def test_robust_transformer_pipeline_df(self):
-        input_types = ('ser', 'df',)
-        bad_input_checks = (
-            lambda x: isinstance(x, pd.Series),
-            lambda x: isinstance(x, pd.DataFrame),
-        )
-        catches = (ValueError, TypeError)
-        self._test_robust_transformer_pipeline(
-            input_types, bad_input_checks, catches)
-
-    def test_robust_transformer_pipeline_arr(self):
-        input_types = ('ser', 'df', 'arr1d')
-        bad_input_checks = (
-            lambda x: isinstance(x, pd.Series),
-            lambda x: isinstance(x, pd.DataFrame),
-            lambda x: isinstance(x, np.ndarray) and x.ndim == 1,
-        )
-        catches = (ValueError, TypeError)
-        self._test_robust_transformer_pipeline(
-            input_types, bad_input_checks, catches)
-
-    def test_robust_transformer_pipeline_str_repr(self):
-        robust_transformer = make_robust_transformer([IdentityTransformer()])
-        for func in [str, repr]:
-            s = func(robust_transformer)
-            assert len(s) > 0
+@pytest.mark.parametrize(
+    'robust_maker',
+    [
+        DelegatingRobustTransformer,
+        lambda x: make_robust_transformer([x]),
+    ]
+)
+def test_robust_str_repr(robust_maker):
+    robust_transformer = robust_maker(IdentityTransformer())
+    for func in [str, repr]:
+        s = func(robust_transformer)
+        assert len(s) > 0
