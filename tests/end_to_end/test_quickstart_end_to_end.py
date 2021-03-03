21cbef17208db1808e4cb773b836744703d9a45d
Micah Smith <micahjsmith@gmail.com>
2021-03-03 10:39:44 -0500

    cleanup

--- tests/end_to_end/test_quickstart_end_to_end.py
+++ tests/end_to_end/test_quickstart_end_to_end.py
@@ -5,11 +5,12 @@ from ballet.util import work_in
 
 @pytest.mark.slow
 def test_quickstart_install(quickstart, virtualenv):
+    """Test that we can install the project resulting from quickstart"""
     # This is an annoying test because the project template should specify
     # as installation dependency the most recent tagged version of ballet,
     # but this will not necessarily have been released on PyPI
     # TODO: figure out the right way to mange this
-    d = quickstart.tempdir.joinpath(quickstart.project_slug).absolute()
+    d = quickstart.tempdir.joinpath(quickstart.project_slug).resolve()
     with work_in(d):
         # cmd = 'cd "{d!s}" && invoke install'.format(d=d)
         cmd = 'echo okay'
