5d57e070a659e4b3c85c9c1a861c299e54055761
Micah Smith <micahjsmith@gmail.com>
2021-03-03 09:49:07 -0500

    rewrite test_accepters

--- tests/validation/test_accepters.py
+++ tests/validation/test_accepters.py
@@ -1,5 +1,6 @@
-import unittest
 from unittest.mock import patch
+
+import pytest
 
 from ballet.eng.misc import IdentityTransformer
 from ballet.feature import Feature
@@ -8,61 +9,58 @@ from ballet.validation.feature_acceptance.validator import (
 from tests.util import load_regression_data
 
 
-class NoOpAccepterTest(unittest.TestCase):
+def test_noop_accepter():
+    X_df = y_df = y = None
+    existing_features = []
+    feature = None
 
-    def test_accepter(self):
-        X_df = y_df = y = None
-        existing_features = []
-        feature = None
+    expected = False
 
-        expected = False
+    accepter = NeverAccepter(X_df, y_df, y, existing_features, feature)
+    actual = accepter.judge()
 
-        accepter = NeverAccepter(X_df, y_df, y, existing_features, feature)
-        actual = accepter.judge()
-
-        assert expected == actual
+    assert expected == actual
 
 
-class RandomAccepterTest(unittest.TestCase):
+@patch('random.uniform', return_value=0.0)  # makes sure feature is accepted
+def test_random_accepter(mock_uniform):
+    X_df = y_df = y = None
+    existing_features = []
+    candidate_feature = None
 
-    @patch('random.uniform')
-    def test_accepter(self, mock_uniform):
-        X_df = y_df = y = None
-        existing_features = []
-        candidate_feature = None
+    expected = True
 
-        expected = True
+    accepter = RandomAccepter(
+        X_df, y_df, y, existing_features, candidate_feature)
+    actual = accepter.judge()
 
-        mock_uniform.return_value = 0.0  # makes sure feature is accepted
-
-        accepter = RandomAccepter(
-            X_df, y_df, y, existing_features, candidate_feature)
-        actual = accepter.judge()
-
-        assert expected == actual
+    assert expected == actual
 
 
-class GFSSFAccepterTest(unittest.TestCase):
+@pytest.fixture
+def sample_data():
+    X_df, y_df = load_regression_data(
+        n_informative=1, n_uninformative=14)
+    y = y_df
+    return X_df, y_df, y
 
-    def setUp(self):
-        self.X_df, self.y_df = load_regression_data(
-            n_informative=1, n_uninformative=14)
-        self.y = self.y_df
 
-    def test_init(self):
-        feature_1 = Feature(
-            input='A_0',
-            transformer=IdentityTransformer(),
-            source='1st Feature')
-        feature_2 = Feature(
-            input='Z_0',
-            transformer=IdentityTransformer(),
-            source='2nd Feature')
+def test_gfssf_accepter_init(sample_data):
+    X_df, y_df, y = sample_data
 
-        features = [feature_1]
-        candidate_feature = feature_2
+    feature_1 = Feature(
+        input='A_0',
+        transformer=IdentityTransformer(),
+        source='1st Feature')
+    feature_2 = Feature(
+        input='Z_0',
+        transformer=IdentityTransformer(),
+        source='2nd Feature')
 
-        accepter = GFSSFAccepter(
-            self.X_df, self.y_df, self.y, features, candidate_feature)
+    features = [feature_1]
+    candidate_feature = feature_2
 
-        assert accepter is not None
+    accepter = GFSSFAccepter(
+        X_df, y_df, y, features, candidate_feature)
+
+    assert accepter is not None
