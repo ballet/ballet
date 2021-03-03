62adef38f410257c351cc157d290425c8b407105
Micah Smith <micahjsmith@gmail.com>
2021-03-02 20:12:46 -0500

    break out test_git

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,7 +7,6 @@ import tempfile
 import unittest
 from unittest.mock import ANY, mock_open, patch
 
-import git
 import numpy as np
 import pandas as pd
 import pytest
@@ -16,102 +15,12 @@ import ballet
 import ballet.util
 import ballet.util.ci
 import ballet.util.fs
-import ballet.util.git
 import ballet.util.io
 from ballet.util import one_or_raise
 from ballet.util.code import blacken_code, get_source, is_valid_python
 from ballet.util.mod import (  # noqa F401
     import_module_at_path, import_module_from_modname,
     import_module_from_relpath, modname_to_relpath, relpath_to_modname,)
-
-
-class GitTest(unittest.TestCase):
-
-    def test_make_commit_range(self):
-        a = 'abc1234'
-        b = 'def4321'
-        expected_commit_range = 'abc1234...def4321'
-        actual_commit_range = ballet.util.git.make_commit_range(a, b)
-        assert actual_commit_range == expected_commit_range
-
-    @unittest.expectedFailure
-    def test_get_diff_endpoints_from_commit_range(self):
-        raise NotImplementedError
-
-    @unittest.expectedFailure
-    def test_get_repo(self):
-        raise NotImplementedError
-
-    @unittest.expectedFailure
-    def test_get_pr_num(self):
-        raise NotImplementedError
-
-    @unittest.expectedFailure
-    def test_get_branch(self):
-        raise NotImplementedError
-
-    @unittest.expectedFailure
-    def test_switch_to_new_branch(self):
-        raise NotImplementedError
-
-    @unittest.expectedFailure
-    def test_set_config_variables(self):
-        raise NotImplementedError
-
-    @patch('requests.get')
-    def test_get_pull_requests(self, mock_requests_get):
-        owner = 'foo'
-        repo = 'bar'
-        state = 'closed'
-        ballet.util.git.get_pull_requests(owner, repo, state=state)
-
-        (url, ), kwargs = mock_requests_get.call_args
-        assert owner in url
-        assert repo in url
-        assert 'headers' in kwargs
-        assert 'params' in kwargs
-        assert kwargs['params']['state'] == state
-
-    @patch('ballet.util.git.get_pull_requests')
-    def test_get_pull_request_outcomes(self, mock_get_pull_requests):
-        mock_get_pull_requests.return_value = [
-            {
-                'id': 1,
-                "created_at": "2011-01-26T19:01:12Z",
-                "updated_at": "2011-01-26T19:01:12Z",
-                "closed_at": "2011-01-26T19:01:12Z",
-                "merged_at": "2011-01-26T19:01:12Z",
-            },
-            {
-                "created_at": "2011-01-26T19:03:19Z",
-                "updated_at": "2011-01-26T19:03:19Z",
-                "closed_at": "2011-01-26T19:04:01Z",
-                "merged_at": None,
-            }
-        ]
-        owner = 'foo'
-        repo = 'bar'
-
-        expected = ['accepted', 'rejected']
-        actual = ballet.util.git.get_pull_request_outcomes(owner, repo)
-        assert actual == expected
-        mock_get_pull_requests.assert_called_once_with(
-            owner, repo, state='closed')
-
-    def test_did_git_push_succeed(self):
-        local_ref = None
-        remote_ref_string = None
-        remote = None
-
-        flags = 0
-        push_info = git.remote.PushInfo(flags, local_ref, remote_ref_string,
-                                        remote)
-        assert ballet.util.git.did_git_push_succeed(push_info)
-
-        flags = git.remote.PushInfo.ERROR
-        push_info = git.remote.PushInfo(flags, local_ref, remote_ref_string,
-                                        remote)
-        assert not ballet.util.git.did_git_push_succeed(push_info)
 
 
 class IoTest(unittest.TestCase):
---
+++ tests/util/test_git.py
@@ -0,0 +1,103 @@
+from unittest.mock import patch
+import git
+import pytest
+
+from ballet.util.git import (
+    did_git_push_succeed, get_pull_request_outcomes, get_pull_requests,
+    make_commit_range,)
+
+
+def test_make_commit_range():
+    a = 'abc1234'
+    b = 'def4321'
+    expected_commit_range = 'abc1234...def4321'
+    actual_commit_range = make_commit_range(a, b)
+    assert actual_commit_range == expected_commit_range
+
+
+@pytest.mark.xfail
+def test_get_diff_endpoints_from_commit_range():
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_get_repo():
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_get_pr_num():
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_get_branch():
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_switch_to_new_branch():
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_set_config_variables():
+    raise NotImplementedError
+
+
+@patch('requests.get')
+def test_get_pull_requests(mock_requests_get):
+    owner = 'foo'
+    repo = 'bar'
+    state = 'closed'
+    get_pull_requests(owner, repo, state=state)
+
+    (url, ), kwargs = mock_requests_get.call_args
+    assert owner in url
+    assert repo in url
+    assert 'headers' in kwargs
+    assert 'params' in kwargs
+    assert kwargs['params']['state'] == state
+
+
+@patch('ballet.util.git.get_pull_requests')
+def test_get_pull_request_outcomes(mock_get_pull_requests):
+    mock_get_pull_requests.return_value = [
+        {
+            'id': 1,
+            "created_at": "2011-01-26T19:01:12Z",
+            "updated_at": "2011-01-26T19:01:12Z",
+            "closed_at": "2011-01-26T19:01:12Z",
+            "merged_at": "2011-01-26T19:01:12Z",
+        },
+        {
+            "created_at": "2011-01-26T19:03:19Z",
+            "updated_at": "2011-01-26T19:03:19Z",
+            "closed_at": "2011-01-26T19:04:01Z",
+            "merged_at": None,
+        }
+    ]
+    owner = 'foo'
+    repo = 'bar'
+
+    expected = ['accepted', 'rejected']
+    actual = get_pull_request_outcomes(owner, repo)
+    assert actual == expected
+    mock_get_pull_requests.assert_called_once_with(
+        owner, repo, state='closed')
+
+
+def test_did_git_push_succeed():
+    local_ref = None
+    remote_ref_string = None
+    remote = None
+
+    flags = 0
+    push_info = git.remote.PushInfo(flags, local_ref, remote_ref_string,
+                                    remote)
+    assert did_git_push_succeed(push_info)
+
+    flags = git.remote.PushInfo.ERROR
+    push_info = git.remote.PushInfo(flags, local_ref, remote_ref_string,
+                                    remote)
+    assert not did_git_push_succeed(push_info)
